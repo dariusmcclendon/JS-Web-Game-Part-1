756362a24767e6a9848508ec15662fd1a2910f29
Darius <dariusmmcclendon@gmail.com>
2021-12-20 13:47:44 -0500

    added newItem function; refactored code to allow newImage to run inside newItem. Generated items using newItem.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
 /* 
 NewImage function : creates an image
 accepts the src, left and bottom positions as strings
 */
-function newImage(src, posLeft, posBot){
-    let newImg = document.createElement('img')
+function newImage(src, posLeft, posBot, item){
+
+    // this code block allows newImage to be used inside other functions
+    let newImg // create a local variable named newImg
+    if (item === undefined) { //if statement checks to see if the item parameter is defined
+        newImg = document.createElement('img') // assigns document.yadayee to newImg
+    } else {
+        newImg = item; // assigns the item argument passed if defined
+    }
     newImg.src = src
     newImg.style.position = 'fixed'
     newImg.style.left = posLeft
@@ -13,21 +22,32 @@ function newImage(src, posLeft, posBot){
     console.log('NewImage ran : new image generated.')
 }
 
+/*
+NewItem function : creats an item
+accepts the src, left, and bottom positions as strings
+Identical to newImage, but adds eventListener
+*/
+function newItem(src, posLeft, posBot){
+    // create local variable named item
+    let item = document.createElement('img')
+    //passes item as an argument to newImage, newImage will now use item instead of generating its own variable
+    newImage(src, posLeft, posBot, item)
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
+    console.log('NewItem ran : new item generated')    
+}
+
 //using NewImage to create character sprite
 newImage('assets/green-character.gif', '100px', '100px')
 //using NewImage to create pine tree sprite
-newImage('assets/pine-tree.png', '450px', '450px')
-
-// let greenCharacter = document.createElement('img')
-// greenCharacter.src = 'assets/green-character.gif'
-// greenCharacter.style.position = 'fixed'
-// greenCharacter.style.left = '100px'
-// greenCharacter.style.bottom = '100px'
-// document.body.append(greenCharacter)
-
-// let pineTree = document.createElement('img')
-// pineTree.src = 'assets/pine-tree.png'
-// pineTree.style.position = 'fixed'
-// pineTree.style.left = '450px'
-// pineTree.style.bottom = '200px'
-// document.body.append(pineTree)
+newImage('assets/pine-tree.png', '450px', '200px')
+//using NewImage to create.. so on and so forth
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px', '425px')
+//NewItem block
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/sheild.png','165px', '185px')
+newItem('assets/staff.png','600px', '100px')

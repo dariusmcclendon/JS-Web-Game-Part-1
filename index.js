9be234314c9f29fd544a4940491ea8095377fdfa
Darius <dariusmmcclendon@gmail.com>
2021-12-20 14:50:56 -0500

    Added drawBackground function!

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 
 let wHeight = window.innerHeight;
 let wWidth = window.innerWidth;
+console.log('Height : ' + wHeight)
+console.log('Width : ' + wWidth)
+
+/*
+drawBackground function : draws background images to cover screen
+*/
+
+function drawBackground(src,rowSize, columnSize){
+    
+    for(let c = 0; c < columnSize ; c++){
+        for(let i = 0; i < rowSize ; i++){
+            let tile = document.createElement('img')
+            newImage(src, (i*100).toString() + 'px', (c*100).toString() + 'px', tile)
+        }
+    }
+    
+}
 /* 
 NewImage function : creates an image
 accepts the src, left and bottom positions as strings
 */
-function newImage(src, posLeft, posBot, item){
+function newImage(src, posLeft, posBot, img){
 
     // this code block allows newImage to be used inside other functions
     let newImg // create a local variable named newImg
-    if (item === undefined) { //if statement checks to see if the item parameter is defined
+    if (img === undefined) { //if statement checks to see if the item parameter is defined
         newImg = document.createElement('img') // assigns document.yadayee to newImg
     } else {
-        newImg = item; // assigns the item argument passed if defined
+        newImg = img; // assigns the item argument passed if defined
     }
     newImg.src = src
     newImg.style.position = 'fixed'
@@ -37,7 +54,12 @@ function newItem(src, posLeft, posBot){
     })
     console.log('NewItem ran : new item generated')    
 }
+//draw the sky first
+drawBackground('assets/sky.png', 100, 15)
+//draw grass
+drawBackground('assets/grass.png', 100, 5)
 
+//draw rest of the images over it
 //using NewImage to create character sprite
 newImage('assets/green-character.gif', '100px', '100px')
 //using NewImage to create pine tree sprite
@@ -51,3 +73,4 @@ newImage('assets/well.png','500px', '425px')
 newItem('assets/sword.png', '500px', '405px')
 newItem('assets/sheild.png','165px', '185px')
 newItem('assets/staff.png','600px', '100px')
+

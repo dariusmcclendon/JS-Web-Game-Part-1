ebb3d6d63c88ffbcc93d298a015d76a8e8396a71
Darius <dariusmmcclendon@gmail.com>
2021-12-21 19:18:23 -0500

    Added code for Part II

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 
 let wHeight = window.innerHeight;
 let wWidth = window.innerWidth;
-console.log('Height : ' + wHeight)
-console.log('Width : ' + wWidth)
+let inventory = document.createElement('div')
+// console.log('Height : ' + wHeight)
+// console.log('Width : ' + wWidth)
+
+/*
+makeInventory function : creates inventory div,  
+*/
+
+function makeInventory(){
+    
+    inventory.style.position = 'fixed'
+    inventory.style.left = '0px'
+    inventory.style.bottom = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+    console.log('Inventory generated')
+}
+
+
+
 
 /*
 drawBackground function : draws background images to cover screen
@@ -53,11 +78,17 @@ function newItem(src, posLeft, posBot){
     let item = document.createElement('img')
     //passes item as an argument to newImage, newImage will now use item instead of generating its own variable
     newImage(src, posLeft, posBot, item)
-    item.addEventListener('dblclick', function(){
+    item.addEventListener('dblclick', addToInventory)
+    console.log('NewItem ran : new item generated')
+    function addToInventory(){
         item.remove()
-    })
-    console.log('NewItem ran : new item generated')    
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = src
+        inventory.append(inventoryItem)
+    }    
 }
+
+
 //draw the sky first
 drawBackground('assets/sky.png', 100, 15)
 //draw grass
@@ -77,4 +108,5 @@ newImage('assets/well.png','500px', '425px')
 newItem('assets/sword.png', '500px', '405px')
 newItem('assets/sheild.png','165px', '185px')
 newItem('assets/staff.png','600px', '100px')
+makeInventory()
 
